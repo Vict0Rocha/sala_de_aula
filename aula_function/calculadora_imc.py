5f60ea99497f7caefae33e917cbcabc8be4eee87
victor <victorhugoocordeirorocha@gmail.com>
2023-08-31 21:44:05 -0400

    Importando funções de outros arquivos

---
+++ aula_function/calculadora_imc.py
@@ -0,0 +1,4 @@
+from func_imc import imc, nome #Importando a função de outro arquivo
+
+nome()
+imc()
---
+++ aula_function/func_imc.py
@@ -0,0 +1,7 @@
+def imc():
+    peso = float(input('Digite seu peso: '))
+    altura = float(input('Digite sua altura: '))
+    return peso / (altura**2)
+
+def nome():
+    return input('Digite seu nome: ')
