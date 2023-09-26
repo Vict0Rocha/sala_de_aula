36d60af89dffc5ddc301a21643070f04f9fdcca6
victor <victorhugoocordeirorocha@gmail.com>
2023-09-25 21:00:04 -0400

    Função recursiva

--- aula_function/recursividade.py
+++ aula_function/recursividade.py
@@ -1,7 +1,18 @@
-def fatorial(num):
-    if num == 1:
-        return 1
-    return num * fatorial(num-1)
+# def fatorial(num):
+#     if num == 1:
+#         return 1
+#     return num * fatorial(num-1)
 
 
-print(fatorial(5))
+# print(fatorial(5))
+
+def func(qtd: int) -> int:
+    if qtd <= 1:
+        return print("Hello, World!")
+    print("Hello, World!")
+    return func(qtd - 1)
+
+
+if __name__ == "__main__":
+    # print("Hello, world!")
+    func(10)
