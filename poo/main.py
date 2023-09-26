913c1c3daba3eab9d27d828dbcc8fc03bec36e16
victor <victorhugoocordeirorocha@gmail.com>
2023-09-25 22:25:25 -0400

    POO - Atividade de sobre pessoas

--- poo/main.py
+++ poo/main.py
@@ -2,4 +2,6 @@ from pessoa import Pessoa
 
 humano1 = Pessoa('Adão', 'verde', 'preto', 1.80, 'masculino')
 humano1.falar()
-# humano.falar()
+
+humano2 = Pessoa('Adão', 'verde', 'preto', 1.80, 'masculino')
+humano2.andar()
