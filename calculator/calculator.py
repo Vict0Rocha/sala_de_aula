8a08e015517bfd48ac93e5ce82905b371a39bcb1
victor <victorhugoocordeirorocha@gmail.com>
2023-09-28 21:30:35 -0400

    Calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,6 @@
+class Calculadora_padrao:
+    def __init__(self, pesso, valor, cor):
+        self.pesso = pesso
+        self.valor = valor
+        self.cor = cor
+        print(pesso, valor, cor)
---
+++ calculator/main.py
@@ -0,0 +1,5 @@
+from calculator import Calculadora_padrao
+
+calculator1 = Calculadora_padrao('2Kg', 'R$5.00', 'Roxo')
+
+print(calculator1)
