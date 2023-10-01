9b7e553d0db671c51e067d621fcc4ab610b4c221
victor <victorhugoocordeirorocha@gmail.com>
2023-10-01 14:22:04 -0400

    Primeiros passos Criado a classe e algumas funções, como soma, subtração e multiplicação

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -3,4 +3,18 @@ class Calculator:
         self.pesso = pesso
         self.valor = valor
         self.cor = cor
-        print(pesso, valor, cor)
+
+    def screen(self):
+        print('O estilo da calculadora é...')
+        print(f'Cor: {self.cor}')
+        print(f'Pesso: {self.pesso}')
+        print(f'Valor: {self.valor}')
+
+    def soma(self, n1, n2):
+        return print(f'{n1} + {n2} =  {n1 + n2}')
+
+    def subtrair(self, n1, n2):
+        return print(f'{n1} - {n2} =  {n1 - n2}')
+
+    def multiplicar(self, n1, n2):
+        return print(f'{n1} * {n2} =  {n1 * n2}')
--- calculator/main.py
+++ calculator/main.py
@@ -2,5 +2,24 @@ from calculator import Calculator
 
 calculator_padrao = Calculator('2Kg', 'R$5.00', 'Roxo')
 calculator_top = Calculator('500gm', 'R$2.000', 'Rosa')
-print(calculator_padrao)
-print(calculator_top)
+
+# print('Digie 1 - Calculadora Padrão')
+# print('Digie 2 - Calculadora Top')
+
+# opcao = input('Digite o qual calculadora vc que usar: ')
+
+# if opcao == 1:
+#     ...
+
+# elif opcao == 2:
+#     ...
+# else:
+#     print('Opção invalida!')
+
+# calculator_padrao.soma(5, 5)
+# print(calculator_padrao.subtrair(10, 5))
+# print(calculator_padrao.multiplicar(3, 5))
+
+calculator_padrao.screen()
+print(20*'-')
+calculator_top.screen()
