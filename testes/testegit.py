6b6665e1b3953aace4370efee3f109c5b20bab94
victor <victorhugoocordeirorocha@gmail.com>
2023-10-02 22:13:13 -0400

    testes

--- testes/testegit.py
+++ testes/testegit.py
@@ -1,13 +1,46 @@
-# # print('Hello, Git!')
+import os
 
-# numero = int(input("Fatorial de: "))
+lista = []
+condicao = True
 
-# resultado = 1
-# count = 1
+while condicao == True:
+    # print('-'*15)
+    # print('FAÇA UMA LISTA')
+    # print('-'*15)
+    print('\nDigite oque deseja realizar')
+    print('[A]adicionar [D]eletar [L]listar [S]sair')
+    escolha = input(' ')
 
-# while count <= numero:
-#     resultado *= count
-#     count += 1
+    if escolha == 'a':
+        os.system('cls')
+        item = input('Digite o item que deseja <<< ')
+        lista.append(item)
 
-# print(resultado)
+    elif escolha == 'd':
+        os.system('cls')
+        indice_digitado = input('Digite o índice do ítem que deseja remover <<< ')
+        
+        try:
+            indice_remover = int(indice_digitado)
+            del lista[indice_remover]
+        except ValueError:
+            print('[ERRO] - Por favor, digite somente inteiros.')
+        except IndexError:
+            print('[ERRO] - Índice não existe na sua lista.')
+        except Exception:
+            print('[ERRO] - Problema não identificado pelo sistema.')
 
+    elif escolha == 'l':
+         os.system('cls')
+
+         if len(lista) <= 0:
+             print('Nada para lista! ')
+
+         for i, item in enumerate(lista): #Para percorrer indice por indice de uma lista
+             print(i, item)
+
+    elif escolha == 's':
+         break
+    else:
+        print('Digite somente: A, D, ou L')
+        
