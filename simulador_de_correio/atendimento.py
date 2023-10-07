b7a394d9f8e38723c1e3a844c40bbfe33e8f97f6
victor <victorhugoocordeirorocha@gmail.com>
2023-10-06 23:48:25 -0400

    Opção fila finalizada

--- simulador_de_correio/atendimento.py
+++ simulador_de_correio/atendimento.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 class Atendimentos:
@@ -10,6 +11,7 @@ class Atendimentos:
         self.escolha = escolha
         validacao = True
         print('O que você deseja fazer?')
+        print('0 - Finalizar o programa')
         print('1 - Cadastrar')
         print('2 - Listar')
         print('3 - Remover')
@@ -35,35 +37,41 @@ class Atendimentos:
             n_opcao = self.opcao(self)
 
             match n_opcao:
+                case 0:
+                    sys.exit(0)
                 case 1:
                     os.system('cls')
                     print('Digite oque você deseja cadastrar')
                     item_cadastro = input('<<< ')
                     lista.append(item_cadastro)
-                    print(lista)
 
                 case 2:
                     os.system('cls')
                     if len(lista) <= 0:
                         print('Nada para lista')
 
+                    os.system('cls')
                     for i, item_cadastro in enumerate(lista):
-                        print(i, item_cadastro)
+                        print(f'{i}) {item_cadastro}')
+                    print('')
 
                 case 3:
-                    # print('Digite o índice que dejesa revomer')
-                    # indice_digitado = input('<<< ')
+                    print('Digite o índice que dejesa revomer')
+                    indice_digitado = input('<<< ')
 
-                    # try:
-                    #     indice_remover = int(indice_digitado)
-                    #     del lista[indice_remover]
-                    # except ValueError:
-                    #     print('[ERRO] - Por favor, digite somente inteiros.')
-                    # except IndexError:
-                    #     print('[ERRO] - Índice não existe na sua lista.')
-                    ...
+                    try:
+                        indice_remover = int(indice_digitado)
+                        del lista[indice_remover]
+                    except ValueError:
+                        print('[ERRO] - Por favor, digite somente inteiros.')
+                        continue
+                    except IndexError:
+                        print('[ERRO] - Índice não existe na sua lista.')
+                        continue
+
                 case Exception:
                     print('Opção inválida!')
+                    continue
 
     def lista(self):
         print('Você escolheu LISTA')
