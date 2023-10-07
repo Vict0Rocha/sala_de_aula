08a1ce39b32945a55fa19e4b7688ef46d0fab61e
victor <victorhugoocordeirorocha@gmail.com>
2023-10-06 23:30:20 -0400

    Fila feito

--- simulador_de_correio/atendimento.py
+++ simulador_de_correio/atendimento.py
@@ -1,3 +1,6 @@
+import os
+
+
 class Atendimentos:
 
     def __init__(self, nome_pessoa):
@@ -5,64 +8,67 @@ class Atendimentos:
 
     def opcao(self, escolha):
         self.escolha = escolha
+        validacao = True
         print('O que você deseja fazer?')
         print('1 - Cadastrar')
         print('2 - Listar')
         print('3 - Remover')
 
-        escolha = input('Digite seua escolha: ')
+        while validacao == True:
+            escolha = input('Digite sua escolha: ')
+            try:
+                escolha_int = int(escolha)
+                validacao = False
+            except ValueError:
+                print('[ERRO] - Por favor, digite somente números inteiros.')
+                continue
+            except Exception:
+                print('[ERRO] - Problema não identificado pelo sistema.')
+                continue
+            return escolha_int
 
-        try:
-            escolha_int = int(escolha)
-
-        except ValueError:
-            print('[ERRO] - Por favor, digite somente números inteiros.')
-        except Exception:
-            print('[ERRO] - Problema não identificado pelo sistema.')
-        return escolha_int
-
-    def fila(self, lista):
+    def fila(self):
         print('Você escolheu FILA')
-        n_opcao = self.opcao(self)
-        # self.lista = lista
-        # lista = []
+        condicao = True
+        lista = []
+        while condicao == True:
+            n_opcao = self.opcao(self)
 
-        match n_opcao:
-            case 1:
-                print('Digite oque você deseja cadastrar')
-            case 2:
-                print('Sua lista é...')
-            case 3:
-                print('Digite o índice que dejesa revomer')
-            case Exception:
-                print('Opção inválida!')
+            match n_opcao:
+                case 1:
+                    os.system('cls')
+                    print('Digite oque você deseja cadastrar')
+                    item_cadastro = input('<<< ')
+                    lista.append(item_cadastro)
+                    print(lista)
+
+                case 2:
+                    os.system('cls')
+                    if len(lista) <= 0:
+                        print('Nada para lista')
+
+                    for i, item_cadastro in enumerate(lista):
+                        print(i, item_cadastro)
+
+                case 3:
+                    # print('Digite o índice que dejesa revomer')
+                    # indice_digitado = input('<<< ')
+
+                    # try:
+                    #     indice_remover = int(indice_digitado)
+                    #     del lista[indice_remover]
+                    # except ValueError:
+                    #     print('[ERRO] - Por favor, digite somente inteiros.')
+                    # except IndexError:
+                    #     print('[ERRO] - Índice não existe na sua lista.')
+                    ...
+                case Exception:
+                    print('Opção inválida!')
 
     def lista(self):
         print('Você escolheu LISTA')
-        self.opcao(self)
         n_opcao = self.opcao(self)
-
-        match n_opcao:
-            case 1:
-                print('Digite oque você deseja cadastrar')
-            case 2:
-                print('Sua lista é...')
-            case 3:
-                print('Digite o índice que dejesa revomer')
-            case Exception:
-                print('Opção inválida!')
 
     def pilha(self):
         print('Você escolheu PILHA')
-        self.opcao(self)
         n_opcao = self.opcao(self)
-
-        match n_opcao:
-            case 1:
-                print('Digite oque você deseja cadastrar')
-            case 2:
-                print('Sua lista é...')
-            case 3:
-                print('Digite o índice que dejesa revomer')
-            case Exception:
-                print('Opção inválida!')
--- simulador_de_correio/main.py
+++ simulador_de_correio/main.py
@@ -9,15 +9,18 @@ nome = input('Digite seu nome: ')
 pessoa = Atendimentos(nome)
 
 print(f'Olá, {pessoa.nome_pessoa}, como você quer ser atendido?')
-print('[L]- Lista [F]- Fila [P]- Pilha')
+print(' [L]- Lista\n [F]- Fila\n [P]- Pilha')
 esccolha = input('<<< ').upper()
 
 match esccolha:
     case 'L':
+        os.system('cls')
         pessoa.lista()
     case 'F':
+        os.system('cls')
         pessoa.fila()
     case 'P':
+        os.system('cls')
         pessoa.pilha()
     case Exception:
         print('Opção inválida!')
