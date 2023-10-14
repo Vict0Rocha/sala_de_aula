3d8293ddad934c175478e79175cb2ab15a45a230
victor <victorhugoocordeirorocha@gmail.com>
2023-10-13 21:11:39 -0400

    Loop feito

--- lista_telefonica/main.py
+++ lista_telefonica/main.py
@@ -1,10 +1,6 @@
-import os
 # Nesse codigo todo os.system('cls') é para limpar a tela
-
-'''
-    Receber na minha função principal
-    Na minha função cadastrar eu só dou um append
-'''
+import os
+import sys
 
 
 class ListaTelefonica:
@@ -14,23 +10,29 @@ class ListaTelefonica:
         self.lista_telefone = []  # Criando a lista de telefone
 
     def cadastrar(self, lista_de_retorno):
-        # os.system('cls')
+        os.system('cls')
+        self.loop = True
+
+        # Criando minha lista com nome e telefone
         self.lista_de_retorno = lista_de_retorno
-        # lista_desetorno = []  # Criando minha lista com nome e telefone
         self.nome = input('Digite o nome para cadastro: ')
         lista_de_retorno.append(self.nome)
-        self.numero = input('Digite o número para cadastro: ')
 
-        try:  # Validando o número digitado
-            self.telefone = int(self.numero)
-            lista_de_retorno.append(self.telefone)
+        while self.loop:
+            self.numero = input('Digite o número para cadastro: ')
 
-        except ValueError:
-            print('Digite somente números e não coloque espaçõs!')
+            try:  # Validando o número digitado
+                self.telefone = int(self.numero)
+                lista_de_retorno.append(self.telefone)
+                self.loop = False
+
+            except ValueError:
+                print('Digite somente números e não coloque espaçõs!')
+                continue
 
         return lista_de_retorno
 
-    def listar(self):
+    def consultar(self):
         ...
 
 
@@ -40,35 +42,41 @@ print(18 * '-')
 
 nome = input('\nDigite seu nome: ')
 usuario = ListaTelefonica(nome)
+lista = []
 os.system('cls')
+loop = True
 
-print(f'Olá, {usuario.nome}, escolha oque você deseja fazer!')
-print('1 - Cadastrar')
-print('2 - Listar')
-escolha = input('<<< ')
+while loop:
+    print(f'Olá, {usuario.nome}, escolha oque você deseja fazer!')
+    print('0 - Sair')
+    print('1 - Cadastrar')
+    print('2 - Listar')
+    escolha = input('>>> ')
 
-# Tratando o valor digitado pelo usuario
-try:
-    escolha_inteiro = int(escolha)
+    # Tratando o valor digitado pelo usuario
+    try:
+        escolha_inteiro = int(escolha)
 
-    if escolha_inteiro == 1:
-        teste = True
-        lista = []
-        while teste == True:
-            # os.system('cls')
+        if escolha_inteiro == 0:
+            sys.exit(0)
+
+        elif escolha_inteiro == 1:
+            os.system('cls')
             lista_principal = usuario.cadastrar(lista)
             print(lista_principal)
 
-    elif escolha_inteiro == 2:
-        os.system('cls')
-        usuario.listar()
+        elif escolha_inteiro == 2:
+            os.system('cls')
+            usuario.consultar()
 
-    else:
-        print('[ERRO] - Opção inválida!')
+        else:
+            print('[ERRO] - Opção inválida!')
+            continue
 
-except ValueError:
-    print('[ERRO] - Digite somente inteiros!')
+    except ValueError:
+        print('[ERRO] - Digite somente inteiros!')
+        continue
 
-except Exception:
-    # print(NameError)
-    print('[ERRO], problema não intendificado!')
+    except Exception:
+        print('[ERRO], problema não intendificado!')
+        continue
