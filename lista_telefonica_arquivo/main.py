7c7b74283621b2dc5cad5acb545e9e56441fbaa9
victor <victorhugoocordeirorocha@gmail.com>
2023-11-10 22:13:32 -0400

    Atidade sobre arquio

--- lista_telefonica_arquivo/main.py
+++ lista_telefonica_arquivo/main.py
@@ -1,1 +1,16 @@
-print('Hello, World!')
+print(18 * '-')
+print(' LISTA TELEFONICA')
+print(18 * '-')
+print('')
+
+lista = []
+
+print('Digite oque você desejar fazer!')
+print('Cadastrar - 1')
+print('Consultar - 2')
+escolha = input('<<< ')
+
+# if escolha == 1:
+with open('lista_telefonica.txt', 'a', encoding='utf-8') as x:
+    var = input('')
+    x.write(var)
