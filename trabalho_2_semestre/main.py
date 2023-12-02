e739ae18215d4cad3c28ff6270ac1ad586969713
victor <victorhugoocordeirorocha@gmail.com>
2023-12-01 22:34:46 -0400

    Trabalho

--- trabalho_2_semestre/main.py
+++ trabalho_2_semestre/main.py
@@ -1,46 +1,55 @@
+from lista_de_telefones import Lista_Telefonica
 import sys
+# import os
 
 print(18 * '-')
 print(' LISTA TELEFONICA')
 print(18 * '-')
 print('')
 
-print('Digite o número corespondente do que você deseja?')
-print('\n1 - CADASTRAR')
-print('2 - LISTAR')
-print('3 - CONSULTAR')
-print('4 - EDITAR')
-print('5 - EXCLUIR')
-print('6 - SAIR')
-escolha = input('<<< ')
+while True:
+    print('Digite o número corespondente do que você deseja?')
+    print('\n1 - CADASTRAR')
+    print('2 - LISTAR')
+    print('3 - CONSULTAR')
+    print('4 - EDITAR')
+    print('5 - EXCLUIR')
+    print('6 - SAIR')
+    escolha = input('<<< ')
 
-# Convertendo e verificando se o valor digitado é um número inteiro.
-try:
-    escolha_int = int(escolha)
-    match escolha_int:
-        case 1:
-            print('Você escolheu CADASTAR')
+    # Convertendo e verificando se o valor digitado é um número inteiro.
+    try:
+        escolha_int = int(escolha)
+        match escolha_int:
+            case 1:
+                print('Você escolheu CADASTAR')
+                continue
 
-        case 2:       
-            print('Você escolheu LISTAR')
+            case 2:       
+                print('Você escolheu LISTAR')
+                continue
 
-        case 3:
-            print('Você escolheu CONSULTAR')
+            case 3:
+                print('Você escolheu CONSULTAR')
+                continue
 
-        case 4:
-            print('Você escolheu EDITAR')
+            case 4:
+                print('Você escolheu EDITAR')
+                continue
 
-        case 5:
-            print('Você escolheu EXCLUIR')
-        
-        case 6:
-            sys.exit(0) #Finalizando o programa.
+            case 5:
+                print('Você escolheu EXCLUIR')
+                continue
+                
+            case 6:
+                sys.exit(0) #Finalizando o programa.
 
-        case Exception:
-            print('[ERRO] - O valor digitado, não coresponde a NENHUMA OPÇÃO.')
+            case Exception:
+                print('[ERRO] - O valor digitado, não coresponde a NENHUMA OPÇÃO.')
 
-except ValueError:
-    print('[ERRO] - Por favor, digite somente números INTEIROS.')
-except Exception:
-    print('[ERRO] - Porblema não identificado pelo sistema.')
+    except ValueError:
+        print('[ERRO] - Por favor, digite somente números INTEIROS.')
+        continue
+    except Exception:
+        print('[ERRO] - Porblema não identificado pelo sistema.')
 
